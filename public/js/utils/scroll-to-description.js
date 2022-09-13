ae6674aa9a9a1e8eab51539c8892a842f6c8b67b
Arm0ny <63424081+Arm0ny@users.noreply.github.com>
2022-09-13 14:12:02 +0200

    site finished...ish

--- public/js/utils/scroll-to-description.js
+++
@@ -1,7 +0,0 @@
-let link =document.getElementById('discover')
-let descriptionSection = document.getElementById('description')
-let descriptionSectionRect = descriptionSection.getBoundingClientRect()
-
-link.onclick = () => {
-    window.scrollTo(0, descriptionSectionRect.y)
-}
